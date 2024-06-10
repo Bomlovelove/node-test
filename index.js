be6bc4f8afece8e246928394e4dd3f4dbb893914
Bomlovelove <tkdgus1015@naver.com>
2024-06-10 15:43:44 +0900

    test
    hi

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 2000
+
+app.get('/sound/:name', (req, res) => {
+    const { name } = req.params
+
+    if (name == "dog") {
+        res.json({ 'sound': '멍멍' })
+    } else if (name == "cat") {
+        res.json({ 'sound': '야옹' })
+    } else if (name == "pig") {
+        res.json({ 'sound': '꿀꿀' })
+    } else if{
+        res.json({ 'sound': '알수없음'})
+    }    
+})
+
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}')
+})
